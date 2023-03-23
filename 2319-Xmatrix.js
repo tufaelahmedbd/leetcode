4114f62438bc675f5f895898305826f5699d826a
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-03-24 04:55:06 +0600

    2319. Check if Matrix Is X-Matrix

---
+++ 2319-Xmatrix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var checkXMatrix = function (grid) {
+  let n = grid.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i - j === 0 || i + j === n - 1) {
+        if (grid[i][j] === 0) {
+          return false;
+        }
+      } else {
+        if (grid[i][j] !== 0) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
