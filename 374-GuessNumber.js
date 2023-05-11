e712e2731aceca35f07dda8f877ce5f83397fbf7
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-05-11 18:19:54 +0600

    374. Guess Number Higher or Lower

---
+++ 374-GuessNumber.js
@@ -0,0 +1,26 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let start = 0;
+  let end = n - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let pick = guess(mid);
+    if (pick === 0) return mid;
+    else if (pick > 0) start = mid + 1;
+    else end = mid - 1;
+  }
+  return start;
+};
+console.log(guessNumber(10));
