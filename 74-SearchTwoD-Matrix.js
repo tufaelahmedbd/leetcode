fe02d23db44790f25dc1565d5ed6299caab6e7ef
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-06-01 15:54:19 +0600

    74. Search a 2D Matrix

---
+++ 74-SearchTwoD-Matrix.js
@@ -0,0 +1,33 @@
+var searchMatrix = function (matrix, target) {
+  if (!matrix.length || !matrix[0].length) return false;
+
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  while (col >= 0 && row <= matrix.length - 1) {
+    if (matrix[row][col] === target) return true;
+    if (matrix[row][col] > target) col--;
+    if (matrix[row][col] < target) row++;
+  }
+  return false;
+};
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    3
+  )
+);
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    13
+  )
+);
